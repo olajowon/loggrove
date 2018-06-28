e96738f1a30ec86474c1acc7ea336b78cd94a254
周旺 <zhouwang@yunniao.me>
2018-06-28 18:55:51 +0800

    restricted view

--- static/js/local_log_file.js
+++ static/js/local_log_file.js
@@ -14,9 +14,9 @@ function write_new_row(id){
                     data[i]["path"],
                     data[i]["comment"],
                     data[i]["create_time"],
-                    "<button class='btn btn-xs btn-danger perm' " +
+                    "<button class='btn btn-xs btn-danger role2' " +
                                     "onclick='delete_file(" + data[i]["id"] + ")'>删除</button>&nbsp;" +
-                                "<button class='btn btn-xs btn-warning perm' " +
+                                "<button class='btn btn-xs btn-warning role2' " +
                                     "onclick='open_update_file_modal(" + data[i]["id"] + ")'>编辑</button>&nbsp;" +
                                 "<button class='btn btn-xs btn-info' " +
                                     "onclick='open_chart_modal(" + data[i]["id"] + ")'>图表</button>&nbsp;" +
